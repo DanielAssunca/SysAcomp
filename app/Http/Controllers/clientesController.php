b23c9817b50da7d05b7d8ef884a9c68ac20ba2df
daniel <daniel@itg.com>
2020-03-22 17:12:03 -0300

    vitoria

--- app/Http/Controllers/clientesController.php
+++ app/Http/Controllers/clientesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\clientes;
 use Illuminate\Http\Request;
 
 class clientesController extends Controller
@@ -23,7 +24,7 @@ class clientesController extends Controller
      */
     public function create()
     {
-        //
+        return view('clientes.create');
     }
 
     /**

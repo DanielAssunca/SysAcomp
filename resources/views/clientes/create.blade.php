d3da185bf04efa1765bb9ee06cc0a007dbe10c63
daniel <daniel@itg.com>
2020-03-22 22:42:24 -0300

    TESTE

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -52,37 +52,43 @@
             <hr>
 
                 <div class="form-group">
-                    <label for="logradouroEndereco">Logradouro</label>
-                    <input type="text" class="form-control" placeholder="Logradouro" required name="logradouroEndereco">
-                </div>
-            <div class="row">
-                <div class="col-md-10">
-                    <div class="form-group">
-                        <label for="bairroEndereco">Bairro</label>
-                        <input type="text" class="form-control" placeholder="Bairro" required name="bairroEndereco">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label for="cepEndereco">CEP</label>
+                                <input type="text" class="form-control" placeholder="CEP" required name="cepEndereco">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="logradouroEndereco">Logradouro</label>
+                                <input type="text" class="form-control" placeholder="Logradouro" required name="logradouroEndereco">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="cidadeEndereco">Cidade</label>
+                                <input type="text" class="form-control" placeholder="Cidade" required name="cidadeEndereco">
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="bairroEndereco">Bairro</label>
+                                <input type="text" class="form-control" placeholder="Bairro" required name="bairroEndereco">
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label for="numero">Número</label>
+                                <input type="number" class="form-control" placeholder="Número" required name="numeroEndereco">
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-2">
-                    <div class="form-group">
-                        <label for="numero">Número</label>
-                        <input type="number" class="form-control" placeholder="Número" required name="numeroEndereco">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="cidadeEndereco">Cidade</label>
-                        <input type="text" class="form-control" placeholder="Cidade" required name="cidadeEndereco">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="cepEndereco">CEP</label>
-                        <input type="text" class="form-control" placeholder="CEP" required name="cepEndereco">
-                    </div>
-                </div>
-            </div>
+
             <a href="{{ url()->previous() }}" class="btn btn-default">Voltar</a>
             <button type="submit" class="btn btn-primary">Cadastrar</button>
         </form>

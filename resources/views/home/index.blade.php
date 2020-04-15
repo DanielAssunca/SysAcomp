eed06a0aca464ad2e334f9314756186ef3014093
DanielAssunca <danielsa135@gmail.com>
2020-04-15 08:10:58 -0300

    ASD

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,7 +11,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">SysAcompXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</a>
+                <a class="navbar-brand" href="#">SysAcompXXXXXXXXXXXTESTE TESTE TESTEXXXXXXXXXXXXX</a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">

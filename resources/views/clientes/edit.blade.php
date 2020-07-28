edb60b81c457cf6388846b933a7391e06dc61d56
Daniel Assuncao <danielsa135@gmail.com>
2020-07-28 11:06:11 -0300

    sdfsd

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -12,7 +12,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">SysAcompASASASASSASASASA</a>
+                <a class="navbar-brand" href="#">SysAcomp54564564564546545654</a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
